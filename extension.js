0da8de9323da327e5ba248f245fae8565f552f75
Pamal-Ranasinghe <68419474+Pamal-Ranasinghe@users.noreply.github.com>
2023-12-28 01:47:02 +0530

    push the copied values to array

--- extension.js
+++ extension.js
@@ -4,24 +4,28 @@ const vscode = require('vscode');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+.hello{
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ttOne" is now active!');
+	let valueArray = [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ttOne.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello VS Code');
+	let disposable = vscode.commands.registerCommand('ttOne.helloWorld', async () => {
+		try{
+			let clipboardText = await vscode.env.clipboard.readText();
+			vscode.window.showInformationMessage('Copied value: ' + clipboardText);
+			valueArray.push(clipboardText);
+			console.log(valueArray);
+			clipboardText='';
+		} catch (error){
+			// vscode.window.showErrorMessage('Error reading clipboard: ' + error.message);
+			console.log('Error : ' + error.message);
+		}
+		
 	});
 
 	context.subscriptions.push(disposable);
