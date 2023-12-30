63f964619e9e53f8d7eab757f068484a2e9905e4
Pamal-Ranasinghe <68419474+Pamal-Ranasinghe@users.noreply.github.com>
2023-12-30 16:28:16 +0530

    initial launch

--- extension.js
+++ extension.js
@@ -5,19 +5,17 @@ const vscode = require("vscode");
  */
 function activate(context) {
   try {
-    console.log('Congratulations, your extension "ttOne" is now active!');
+    const outputChannel = vscode.window.createOutputChannel('10xCopyOutput');
     const valueArray = [];
+    let clipboardText = '';
 
     let copy = vscode.commands.registerCommand("ttOne.copy", async () => {
       try {
-        let clipboardText = await vscode.env.clipboard.readText();
-        console.log(clipboardText.length);
+         clipboardText = await vscode.env.clipboard.readText();
         if (clipboardText.length != 0) {
           if (valueArray.length < 10) {
             valueArray.push(clipboardText);
-            vscode.window.showInformationMessage(
-              "Copied value: " + clipboardText
-            );
+            vscode.window.showInformationMessage("Clipboard value moved to 10xCopy : " + clipboardText);
           } else {
             vscode.window.showWarningMessage("10xCopy limit exceeded");
           }
@@ -31,32 +29,43 @@ function activate(context) {
       }
     });
 
-    let listAllValues = vscode.commands.registerCommand(
-      "ttOne.list",
-      async () => {
+    let listAllValues = vscode.commands.registerCommand("ttOne.list",async () => {
         const quickPick = vscode.window.createQuickPick();
 
         const quickPickItems = valueArray.map((option, index) => ({
           label: `value ${index + 1}`,
           description: option,
-		  buttons: [
-			{
-				iconPath: new vscode.ThemeIcon('eye'), // Use an eye icon as an example
-				tooltip: 'View Details',
-			}
-		  ]
+          buttons: [
+            {
+              iconPath: new vscode.ThemeIcon('eye'), // Use an eye icon as an example
+              tooltip: 'View Details'
+            }
+		      ] 
         }));
 
         quickPick.items = quickPickItems;
 
+
         quickPick.placeholder = "Select an option";
 
+       
         quickPick.show();
+
+        quickPick.onDidTriggerItemButton(async (button) => {
+        
+          vscode.window.showInformationMessage(`${button.item.description}`);
+
+          const details = `${button.item.description}`;
+          outputChannel.replace(details);
+          outputChannel.show(true);
+          context.subscriptions.push(outputChannel);
+        });
 
         quickPick.onDidChangeSelection((selection) => {
           if (selection && selection[0]) {
             const selectedLabel = selection[0].description;
             const selectedIndex = valueArray.indexOf(selectedLabel);
+
             const editor = vscode.window.activeTextEditor;
 
             if (editor) {
@@ -70,11 +79,8 @@ function activate(context) {
               valueArray.splice(selectedIndex, 1);
             }
           }
-        });
 
-		quickPick.onDidTriggerButton((button,item) => {
-			console.log("Hellp");
-		});
+        });
 
         quickPick.onDidHide(() => quickPick.dispose());
       }
