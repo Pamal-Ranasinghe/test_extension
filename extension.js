38883720137a1375f59b3c62861e935edafccd4f
Pamal-Ranasinghe <68419474+Pamal-Ranasinghe@users.noreply.github.com>
2024-01-08 01:18:05 +0530

    copy delete buttons are created

--- extension.js
+++ extension.js
@@ -5,17 +5,19 @@ const vscode = require("vscode");
  */
 function activate(context) {
   try {
-    const outputChannel = vscode.window.createOutputChannel('10xCopyOutput');
+    const outputChannel = vscode.window.createOutputChannel("10xCopyOutput");
     const valueArray = [];
-    let clipboardText = '';
+    let clipboardText = "";
 
     let copy = vscode.commands.registerCommand("ttOne.copy", async () => {
       try {
-         clipboardText = await vscode.env.clipboard.readText();
+        clipboardText = await vscode.env.clipboard.readText();
         if (clipboardText.length != 0) {
           if (valueArray.length < 10) {
             valueArray.push(clipboardText);
-            vscode.window.showInformationMessage("Clipboard value moved to 10xCopy : " + clipboardText);
+            vscode.window.showInformationMessage(
+              "Clipboard value moved to 10xCopy : " + clipboardText
+            );
           } else {
             vscode.window.showWarningMessage("10xCopy limit exceeded");
           }
@@ -28,44 +30,73 @@ function activate(context) {
         );
       }
     });
+    
 
-    let listAllValues = vscode.commands.registerCommand("ttOne.list",async () => {
+    let listAllValues = vscode.commands.registerCommand(
+      "ttOne.list",
+      async () => {
         const quickPick = vscode.window.createQuickPick();
 
         const quickPickItems = valueArray.map((option, index) => ({
-          label: `value ${index + 1}`,
-          description: option,
+          // label: `value ${index + 1}`,
+          label: `${option}`,
+          // Fdescription: option,
           buttons: [
             {
-              iconPath: new vscode.ThemeIcon('eye'), // Use an eye icon as an example
-              tooltip: 'View Details'
-            }
-		      ] 
+              iconPath: new vscode.ThemeIcon("eye"), // Use an eye icon as an example
+              command: "command.view",
+              tooltip: "View",
+            },
+            {
+              iconPath: new vscode.ThemeIcon("trash"),
+              command: "command.trash",
+              tooltip: "Remove",
+            },
+            {
+              iconPath: new vscode.ThemeIcon("file"),
+              command: "command.copy",
+              tooltip: "Copy",
+            },
+          ],
         }));
 
         quickPick.items = quickPickItems;
 
-
         quickPick.placeholder = "Select an option";
 
-       
         quickPick.show();
 
         quickPick.onDidTriggerItemButton(async (button) => {
-        
-          vscode.window.showInformationMessage(`${button.item.description}`);
+          const details = `${button.item.label}`;
+          const selectedIndexTriggerButton = valueArray.indexOf(details);
 
-          const details = `${button.item.description}`;
-          outputChannel.replace(details);
-          outputChannel.show(true);
-          context.subscriptions.push(outputChannel);
+          if (button.button.tooltip == "View") {
+            
+            outputChannel.replace(details);
+            outputChannel.show(true);
+            context.subscriptions.push(outputChannel);
+
+          } else if (button.button.tooltip == "Remove") {
+
+            if (selectedIndexTriggerButton !== -1) {
+              valueArray.splice(selectedIndexTriggerButton, 1);
+            }
+            quickPick.dispose();
+            vscode.window.showWarningMessage('Option Deleted');
+
+          } else if (button.button.tooltip == "Copy") {
+            vscode.env.clipboard.writeText(details)
+            vscode.window.showInformationMessage(`Value Copied`);
+          } else {
+            vscode.window.showErrorMessage(`Something went wrong`);
+          }
         });
 
         quickPick.onDidChangeSelection((selection) => {
           if (selection && selection[0]) {
-            const selectedLabel = selection[0].description;
-            const selectedIndex = valueArray.indexOf(selectedLabel);
-
+            // const selectedLabel = selection[0].description;
+            const selectedLabel = selection[0].label;
+           
             const editor = vscode.window.activeTextEditor;
 
             if (editor) {
@@ -74,12 +105,7 @@ function activate(context) {
               });
               quickPick.dispose();
             }
-
-            if (selectedIndex !== -1) {
-              valueArray.splice(selectedIndex, 1);
-            }
           }
-
         });
 
         quickPick.onDidHide(() => quickPick.dispose());
